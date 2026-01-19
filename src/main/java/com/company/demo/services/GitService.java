ccabf3b52425cc8b78b94f46ca68038f61de0fe9
udaykumar000 <udaybingundi@gmail.com>
2026-01-19 21:02:06 +0530

    JGit implemented

--- src/main/java/com/company/demo/services/GitService.java
+++ src/main/java/com/company/demo/services/GitService.java
@@ -3,6 +3,8 @@ package com.company.demo.services;
 import com.company.demo.exceptions.GitOperationException;
 import com.company.demo.utils.DirectoryDeleter;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.Files;
@@ -26,39 +28,56 @@ public class GitService {
             if (Files.exists(clonePath)) {
                 DirectoryDeleter.deleteDirectory(clonePath);
                 log.info("Repository already exists at {}, skipping clone", targetDir);
-                return;
             }
 
-            Files.createDirectories(clonePath.getParent() != null
-                    ? clonePath.getParent()
-                    : clonePath);
+            if (clonePath.getParent() != null) {
+                Files.createDirectories(clonePath.getParent());
+            }
+
+//            Files.createDirectories(clonePath.getParent() != null
+//                    ? clonePath.getParent()
+//                    : clonePath);
 
             log.info("Cloning repository {} into {}", repoUrl, targetDir);
 
+            Git.cloneRepository()
+                    .setURI(repoUrl)
+                    .setDirectory(clonePath.toFile())
+                    .setCloneAllBranches(false)
+                    .setDepth(1)
+                    .call()
+                    .close();
+
+
             // Git clone command
-            ProcessBuilder pb = new ProcessBuilder(
-                    "git", "clone", "--depth", "1", repoUrl, targetDir
-            );
+//            ProcessBuilder pb = new ProcessBuilder(
+//                    "git", "clone", "--depth", "1", repoUrl, targetDir
+//            );
 
 
-            pb.redirectErrorStream(true);
-            Process process = pb.start();
+//            pb.redirectErrorStream(true);
+//            Process process = pb.start();
 
             // Wait with timeout
-            boolean completed = process.waitFor(CLONE_TIMEOUT_MINUTES, TimeUnit.MINUTES);
+//            boolean completed = process.waitFor(CLONE_TIMEOUT_MINUTES, TimeUnit.MINUTES);
 
-            if (!completed) {
-                process.destroyForcibly();
-                throw new GitOperationException("Git clone timed out for "+repoUrl,null);
-            }
+//            if (!completed) {
+//                process.destroyForcibly();
+//                throw new GitOperationException("Git clone timed out for "+repoUrl,null);
+//            }
 
-            if (process.exitValue() != 0) {
-                String errorOutput = new String(process.getInputStream().readAllBytes());
-                throw new GitOperationException("Git clone failed for "+repoUrl+". Error: "+errorOutput,null);
-            }
+//            if (process.exitValue() != 0) {
+//                String errorOutput = new String(process.getInputStream().readAllBytes());
+//                throw new GitOperationException("Git clone failed for "+repoUrl+". Error: "+errorOutput,null);
+//            }
 
             log.info("Successfully cloned repository {}",repoUrl);
-        }catch(GitOperationException e){
+        }catch(GitAPIException e){
+            log.error("JGit error cloning repository {}",repoUrl,e);
+            throw new GitOperationException("JGit error cloning repository "+repoUrl,e);
+
+        }
+        catch(GitOperationException e){
             throw e;
         }catch (Exception e){
             log.error("Unexpected error while cloning repository {}",repoUrl,e);
