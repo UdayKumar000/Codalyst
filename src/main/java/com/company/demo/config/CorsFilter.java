a36824375a37f923c3f3b5897457271b74eb1199
udaykumar000 <udaybingundi@gmail.com>
2026-01-27 16:12:01 +0530

    beans error fixed

--- src/main/java/com/company/demo/config/CorsFilter.java
+++ src/main/java/com/company/demo/config/CorsFilter.java
@@ -19,43 +19,36 @@ public class CorsFilter {
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
-    public Filter corsFilter() {
-        return new Filter() {
-            @Override
-            public void doFilter(
-                    ServletRequest request,
-                    ServletResponse response,
-                    FilterChain chain)
-                    throws IOException, ServletException {
+    public Filter doFilter() {
+        return (request, response, chain) -> {
 
-                HttpServletRequest req = (HttpServletRequest) request;
-                HttpServletResponse res = (HttpServletResponse) response;
+            HttpServletRequest req = (HttpServletRequest) request;
+            HttpServletResponse res = (HttpServletResponse) response;
 
-                res.setHeader(
-                        "Access-Control-Allow-Origin",
-                        "https://codelyst.onrender.com"
-                );
-                res.setHeader(
-                        "Access-Control-Allow-Methods",
-                        "GET, POST, PUT, DELETE, OPTIONS"
-                );
-                res.setHeader(
-                        "Access-Control-Allow-Headers",
-                        "Content-Type, Authorization"
-                );
-                res.setHeader(
-                        "Access-Control-Allow-Credentials",
-                        "false"
-                );
+            res.setHeader(
+                    "Access-Control-Allow-Origin",
+                    "https://codelyst.onrender.com"
+            );
+            res.setHeader(
+                    "Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE, OPTIONS"
+            );
+            res.setHeader(
+                    "Access-Control-Allow-Headers",
+                    "Content-Type, Authorization"
+            );
+            res.setHeader(
+                    "Access-Control-Allow-Credentials",
+                    "false"
+            );
 
-                // VERY IMPORTANT
-                if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
-                    res.setStatus(HttpServletResponse.SC_OK);
-                    return;
-                }
-
-                chain.doFilter(request, response);
+            // VERY IMPORTANT
+            if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+                res.setStatus(HttpServletResponse.SC_OK);
+                return;
             }
+
+            chain.doFilter(request, response);
         };
     }
 }
