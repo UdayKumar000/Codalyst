ca7074bf78573191c87ce2cd0eabdeb0c2849031
udaykumar000 <udaybingundi@gmail.com>
2026-03-18 18:27:15 +0530

    cors setup for localhost

--- src/main/java/com/company/codelyst/config/CorsConfig.java
+++ src/main/java/com/company/codelyst/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("https://codelyst.onrender.com")
+                        .allowedOrigins("https://codelyst.onrender.com","http://localhost:5173")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
